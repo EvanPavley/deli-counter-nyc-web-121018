2dce576da0c29898c2fc756da08d1d23e7a1e490
Evan Pavley <epavley@sva.edu>
2018-12-04 16:23:03 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,17 @@
-# Write your code here.
+def line(katz_deli)
+  if katz_deli.size == 0 
+    puts "The line is currently empty."
+  else
+    katz_deli.each_with_index do |name, index|
+      puts "The line is currently:" += "#{index + 1}. #{name}"
+    end
+  end
+end
+
+def take_a_number
+  
+end
+
+def now_serving
+  
+end
