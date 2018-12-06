a98cd2ad0f518d36329136b2b2a232fa75f0db6f
Evan Pavley <epavley@sva.edu>
2018-12-06 13:09:23 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,16 +1,17 @@
-##
 def line(katz_deli)
   ##check if line is empty
-  if katz_deli.empty? == true
+  if katz_deli.empty?
     puts "The line is currently empty."
   else
-      ##shows everyone their current place in line
-      ##The line is currently: 1. Logan 2. Avi 3. Spencer
-    katz_deli.each_with_index.map do |name, index|
-    puts "The line is currently: #{index+1}. #{name}"
+    ##shows everyone their current place in line
+    ##The line is currently: 1. Logan 2. Avi 3. Spencer
+    line_message = "The line is currently:"
+    katz_deli.each.with_index(1) do |name, index|
+      line_message.concat(" #{index}. #{name}")
+    end
+    puts line_message
   end
 end
-
 
 def take_a_number(katz_deli,name)
   ##should add a person to the line
@@ -21,9 +22,10 @@ end
 
 def now_serving(katz_deli)
 ##check if line is empty
-  if katz_deli.empty? == true
+  if katz_deli.empty?
     puts "There is nobody waiting to be served!"
   else
     ##calls out the next person in line and then remove them from the front.
-    puts "Currently serving #{katz_deli.shift}"
+    puts "Currently serving #{katz_deli.shift}."
+  end
 end
